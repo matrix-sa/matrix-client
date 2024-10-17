aa625e6f5fa018d0b5a7f0737e488c1d90b0396f
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2024-10-18 02:32:32 +0300

    Format some graphs

--- src/components/home/HomeReachRate.vue
+++ src/components/home/HomeReachRate.vue
@@ -1,63 +1,62 @@
 <script setup>
-import VueApexCharts from 'vue3-apexcharts'
-import { useTheme } from 'vuetify'
-import { isRtl } from '.'
+  import VueApexCharts from 'vue3-apexcharts'
+  import { useTheme } from 'vuetify'
+  import { isRtl } from '.'
 
+  const props = defineProps({
+    frequency: Array,
+  })
 
-const props = defineProps({
-  frequency: Array,
-})
+  const vuetifyTheme = useTheme()
 
-const vuetifyTheme = useTheme()
+  const series = [{
+    data: props.frequency?.map(item => item.value),
+  }]
 
-const series = [{
-  data: props.frequency?.map(item => item.value),
-}]
+  const chartOptions = computed(() => {
+    const currentTheme = vuetifyTheme.current.value.colors
 
-const chartOptions = computed(() => {
-  const currentTheme = vuetifyTheme.current.value.colors
-  
-  return {
-    chart: {
-      type: 'line',
-      parentHeightOffset: 0,
-      toolbar: { show: false },
-      zoom: {
+    return {
+      chart: {
+        type: 'line',
+        parentHeightOffset: 0,
+        toolbar: { show: false },
+        zoom: {
+          enabled: false,
+        },
+      },
+      grid: {
+        xaxis: { lines: { show: true } },
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: props.frequency?.map(item => item.date),
+        labels: {
+          format: 'd/M',
+        },
+      },
+      yaxis: {
+        opposite: isRtl.value,
+        labels: {
+          offsetX: -15,
+        },
+      },
+      colors: [currentTheme.warning],
+      stroke: { width: 2 },
+      tooltip: {
         enabled: false,
+        shared: false,
+        intersect: true,
+        x: { show: false },
       },
-    },
-    grid: {
-      xaxis: { lines: { show: true } },
-    },
-    xaxis: {
-      type: 'datetime',
-      categories: props.frequency?.map(item => item.date),
-      labels: {
-        format: 'd/M',
-      },
-    },
-    yaxis: {
-      opposite: isRtl.value,
-      labels: {
-        offsetX: -15,
-      },
-    },
-    colors: [currentTheme.warning],
-    stroke: { width: 2 },
-    tooltip: {
-      enabled: false,
-      shared: false,
-      intersect: true,
-      x: { show: false },
-    },
-  }
-})
+    }
+  })
 </script>
 
 <template>
   <VCard
-    :title="$t('reach-rate')"
     :subtitle="$t('last-90-days')"
+    :title="$t('reach-rate')"
   >
     <VCardText>
       <VueApexCharts
