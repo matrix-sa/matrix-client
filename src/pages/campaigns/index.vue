ea1a0d8a0e2acd5f916d05a6c01dd8456d6a6b78
Jamal <Jamal_Ali@outlook.com>
2024-10-20 00:01:59 +0300

    Revert "Add button to open rule modal"
    This reverts commit 5bfe9505fda94e3a4183b3a4dd41d2158fdafcde.

--- src/pages/campaigns/index.vue
+++ src/pages/campaigns/index.vue
@@ -1,4 +1,2 @@
 <script setup></script>
-<template>
-  <p>Campaign go here</p>
-</template>
+ <template></template>
--- src/pages/rules/index.vue
+++ src/pages/rules/index.vue
@@ -1,16 +1,1 @@
-<script setup>
-
-  const bool = ref(false)
-</script>
-
-<template>
-  <v-btn @click="bool = true">
-    Open Dialog
-  </v-btn>
-  <v-dialog v-model="bool" width="auto">
-    <CampaignRuleModal
-      v-model:is-dialog-visible="bool"
-    />
-  </v-dialog>
-
-</template>
+<script setup></script> <template></template>
