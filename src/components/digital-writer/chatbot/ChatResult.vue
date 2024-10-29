53a69038e168390d387327b6f973f8ff5af583b7
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-10-29 16:07:13 +0300

    Handle Scrollbar Of Digital Writer Chat

--- src/components/digital-writer/chatbot/ChatResult.vue
+++ src/components/digital-writer/chatbot/ChatResult.vue
@@ -12,17 +12,34 @@
     },
   })
 
+  const writerWrapper = ref(null)
+
   const emitUpdateMessagesHistory = newMessageHistory => {
     emit('updateMessagesHistory', newMessageHistory)
+    setTimeout(() => {
+      scrollToBottom()
+    }, 10)
   }
 
   const emitPushInFront = newMessageHistory => {
     emit('pushInFront', newMessageHistory)
+    setTimeout(() => {
+      scrollToBottom()
+    }, 10)
   }
+
+  const scrollToBottom = () => {
+    writerWrapper.value.scrollTop = writerWrapper.value.scrollHeight + 1000
+  }
+
+  defineExpose({
+    scrollToBottom,
+  })
+
 </script>
 
 <template>
-  <div class="chat-container pa-4 d-flex flex-column g-6">
+  <div ref="writerWrapper" class="chat-container pa-4 d-flex flex-column g-6">
     <ChatHeader />
     <div class="chat-inner  d-flex flex-column g-6">
       <ExpansionPanel :messages="messagesHistory?.request_questions" />
@@ -33,12 +50,15 @@
       <UserCard v-if="message.role == 'User'" :message="message.content" />
     </div>
 
-    <Form
-      :active-item="activeItem"
-      :chat-id="messagesHistory?.id"
-      @emit-push-in-front="emitPushInFront"
-      @update-messages-history="emitUpdateMessagesHistory"
-    />
+    <div class="form-wrapper">
+      <Form
+        :active-item="activeItem"
+        :chat-id="messagesHistory?.id"
+        @emit-push-in-front="emitPushInFront"
+        @scroll-to-bottom="scrollToBottom"
+        @update-messages-history="emitUpdateMessagesHistory"
+      />
+    </div>
   </div>
 </template>
 
@@ -47,5 +67,39 @@
   background-color: rgb(var(--v-theme-background));
   border-radius: 16px;
   gap: 24px;
+  position: relative;
+  height: calc(95vh);
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #f0f0f0;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #c0c0c0;
+    border-radius: 8px;
+
+    &:hover {
+      background-color: #a0a0a0;
+    }
+  }
+
+  scrollbar-width: thin;
+  scrollbar-color: #c0c0c0 #f0f0f0;
+
+  .form-wrapper {
+    position: sticky;
+    bottom: 0;
+    transform: translateY(20px);
+    width: 100%;
+    left: 0;
+    padding-top: 12px;
+    background-color: rgb(var(--v-theme-background))
+  }
 }
 </style>
