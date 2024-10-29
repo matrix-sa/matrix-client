32352f37cf206726db101720ff818cf0f9ebeab0
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-10-29 18:46:31 +0300

    Finish Digital Writer With All Features

--- src/pages/assistant/writer/index.vue
+++ src/pages/assistant/writer/index.vue
@@ -1,54 +1,61 @@
 <script setup>
-  import { ref } from 'vue'
-  import { useI18n } from 'vue-i18n'
-  import { useBreadcrumbsStore } from '@/stores/useBreadcrumbsStore'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useBreadcrumbsStore } from '@/stores/useBreadcrumbsStore'
 
-  const { t, locale } = useI18n()
-  const { update } = useBreadcrumbsStore()
+const { t, locale } = useI18n()
+const { update } = useBreadcrumbsStore()
 
-  const activeItem = ref(0) // Reactive state for the active item
-  const isShowAnswers = ref(false)
-  const messagesHistory = ref([])
+const activeItem = ref(0) // Reactive state for the active item
+const isShowAnswers = ref(false)
+const messagesHistory = ref([])
 
-  const chatResult = ref(null)
+const chatResult = ref(null)
 
-  const callScrollChatToTop = () => {
-    chatResult?.value?.scrollToBottom() // Call the child method
-  }
+const callScrollChatToTop = () => {
+  chatResult?.value?.scrollToBottom() // Call the child method
+}
 
-  // Correct the method name
-  const updateActiveTab = itemValue => {
-    activeItem.value = itemValue
+// Correct the method name
+const updateActiveTab = (itemValue, isHistory) => {
+  activeItem.value = itemValue
+
+  if (isHistory == undefined) {
     isShowAnswers.value = true
-  }
+  } else {
+    isShowAnswers.value = false
 
-  const updateMessagesHistory = data => {
-    messagesHistory.value = data
-    setTimeout(() => callScrollChatToTop(), 100)
   }
+}
 
-  const pushInFront = data => {
-    messagesHistory.value.messages.push(data)
-  }
+const updateMessagesHistory = data => {
+  messagesHistory.value = data
+  setTimeout(() => callScrollChatToTop(), 100)
+}
 
-  const showAnswers = () => {
-    activeItem.value = 0
-    isShowAnswers.value = true
-  }
+const pushInFront = data => {
+  messagesHistory.value.messages.push(data)
+}
 
-  watch(
-    locale,
-    () => {
-      update([
-        {
-          title: t('digital_writer'),
-          active: false,
-          to: '/assistant/writer/',
-        },
-      ])
-    },
-    { immediate: true }
-  )
+const showAnswers = data => {
+  messagesHistory.value = data
+  activeItem.value = 0
+  isShowAnswers.value = true
+}
+
+watch(
+  locale,
+  () => {
+    update([
+      {
+        title: t('digital_writer'),
+        active: false,
+        to: '/assistant/writer/',
+      },
+    ])
+  },
+  { immediate: true }
+)
 
 </script>
 
@@ -65,14 +72,9 @@
             <Tabs @update-active-tab="updateActiveTab" @update-messages-history="updateMessagesHistory" />
           </v-col>
           <v-col cols="9">
-            <ChatResult
-              v-if="isShowAnswers"
-              ref="chatResult"
-              :active-item="activeItem"
-              :messages-history="messagesHistory"
-              @push-in-front="pushInFront"
-              @update-messages-history="updateMessagesHistory"
-            />
+            <ChatResult v-if="isShowAnswers" ref="chatResult" :active-item="activeItem"
+              :messages-history="messagesHistory" @push-in-front="pushInFront"
+              @update-messages-history="updateMessagesHistory" />
             <Questions v-else :active-item="activeItem" @show-answers="showAnswers" />
           </v-col>
         </v-row>
