b34a07228d2d0ccd47d8db370847caa11fd2daeb
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-10-31 16:53:32 +0300

    Create Charts Of Reports And Fix Grid

--- src/pages/reports/index.vue
+++ src/pages/reports/index.vue
@@ -2,6 +2,7 @@
   <ReportsNavBar />
   <OrderedPlatformsByPerformance :platforms-data="platformsData" />
   <TopAndLeastCampaigns :least-campaigns="leastCampaigns" :top-campaigns="topCampaigns" />
+  <ReportCharts :statistics="statistics" />
   <v-overlay v-model="loading" class="align-center justify-center" persistent>
     <v-progress-circular color="primary" indeterminate size="50" :width="7" />
   </v-overlay>
@@ -15,9 +16,11 @@
   const platformsData = ref([])
   const topCampaigns = ref([])
   const leastCampaigns = ref([])
+  const statistics = ref([])
 
   const { loading } = useRequest(() => ReportsService.get(), {
     onSuccess: res => {
+      statistics.value = res.data.data
       platformsData.value = res.data.data.platforms_performance_records
       topCampaigns.value = res.data.data.top_performing_campaigns
       leastCampaigns.value = res.data.data.least_performing_campaigns
