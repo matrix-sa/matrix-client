0ca02018cb2c12ec499717537f279d2efb269185
Jamal <Jamal_Ali@outlook.com>
2024-11-04 00:28:52 +0300

    Fix Campaigns Style

--- src/pages/campaigns/index.vue
+++ src/pages/campaigns/index.vue
@@ -63,143 +63,145 @@
   )
 </script>
 <template>
-  <div class="filters-container">
-    <div>
-      <v-date-input
-        v-model="dateRange"
-        bg-color="secondary"
-        cancel-text="cancel"
-        class="date-range-input"
-        clearable
-        density="comfortable"
-        flat
-        hide-details
-        :label="t('select_range_date')"
-        multiple="range"
-        ok-text="ok"
-        persistent-clear
-        :prepend-icon="null"
-        prepend-inner-icon="tabler-calendar"
-        variant="solo-filled"
-        @click:clear="dateRange = null"
-      />
+  <div>
+    <div class="filters-container">
+      <div>
+        <v-date-input
+          v-model="dateRange"
+          bg-color="secondary"
+          cancel-text="cancel"
+          class="date-range-input"
+          clearable
+          density="comfortable"
+          flat
+          hide-details
+          :label="t('select_range_date')"
+          multiple="range"
+          ok-text="ok"
+          persistent-clear
+          :prepend-icon="null"
+          prepend-inner-icon="tabler-calendar"
+          variant="solo-filled"
+          @click:clear="dateRange = null"
+        />
+      </div>
+      <div>
+        <v-text-field
+          v-model="search"
+          class="search-campaign-input"
+          clearable
+          density="comfortable"
+          height="48px"
+          hide-details
+          :label="t('look_for_campaign')"
+          prepend-inner-icon="tabler-search"
+          variant="outlined"
+          @click:clear="search = null"
+        />
+      </div>
     </div>
-    <div>
-      <v-text-field
-        v-model="search"
-        class="search-campaign-input"
-        clearable
-        density="comfortable"
-        height="48px"
-        hide-details
-        :label="t('look_for_campaign')"
-        prepend-inner-icon="tabler-search"
-        variant="outlined"
-        @click:clear="search = null"
-      />
-    </div>
+    <v-card>
+
+      <v-tabs v-model="tab" bg-color="secondary" color="warning" :grow="true">
+        <v-tab
+          base-color="secondary"
+          :text="`(${selectedCampaigns.length}) ` + $t('campaigns')"
+          value="campaigns"
+          variant="elevated"
+        />
+        <v-tab
+          base-color="secondary"
+          :text="`(${selectedAdGroups.length}) ` + $t('ad_groups')"
+          value="ad-groups"
+          variant="elevated"
+        />
+        <v-tab
+          base-color="secondary"
+          :text="$t('ads')"
+          value="ads"
+          variant="elevated"
+        />
+      </v-tabs>
+
+      <v-card-text>
+        <v-tabs-window v-model="tab">
+          <v-tabs-window-item value="campaigns">
+            <VCard>
+              <div class="d-flex align-center justify-space-between ga-4 pa-4">
+                <VCardTitle class="font-weight-medium text-surface-variant pa-0">
+                  {{ $t("campaigns") }}
+                </VCardTitle>
+                <VBtn color="warning" rounded :to="{ name: '/campaigns/add' }">
+                  <VIcon icon="tabler-circle-plus-filled" />
+                  {{ $t("add_campaign") }}
+                </VBtn>
+              </div>
+
+              <VCardText class="pa-4 pt-0">
+                <VDivider />
+                <CampaignsTable
+                  :date-range="dateRange"
+                  :search="search"
+                  @selection-updated="selectedCampaigns = $event"
+                />
+              </VCardText>
+            </VCard>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="ad-groups">
+            <VCard>
+              <div class="d-flex align-center justify-space-between ga-4 pa-4">
+                <VCardTitle class="font-weight-medium text-surface-variant pa-0">
+                  {{ $t("ad_groups") }}
+                </VCardTitle>
+                <VBtn
+                  color="warning"
+                  :disabled="selectedCampaigns.length !== 1"
+                  rounded
+                  :to="addAdGroupRoute"
+                >
+                  <VIcon icon="tabler-circle-plus-filled" />
+                  {{ $t("add_ad_group") }}
+                </VBtn>
+              </div>
+
+              <VCardText class="pa-4 pt-0">
+                <VDivider />
+                <AdsGroupsTable
+                  :campaigns-ids="selectedCampaignsIds"
+                  @selection-updated="selectedAdGroups = $event"
+                />
+              </VCardText>
+            </VCard>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="ads">
+            <VCard>
+              <div class="d-flex align-center justify-space-between ga-4 pa-4">
+                <VCardTitle class="font-weight-medium text-surface-variant pa-0">
+                  {{ $t("ads") }}
+                </VCardTitle>
+                <VBtn
+                  color="warning"
+                  :disabled="selectedAdGroups.length !== 1"
+                  rounded
+                  :to="addAdRoute"
+                >
+                  <VIcon icon="tabler-circle-plus-filled" />
+                  {{ $t("add_ad") }}
+                </VBtn>
+              </div>
+
+              <VCardText class="pa-4 pt-0">
+                <VDivider />
+                <AdsTable :ad-groups-ids="selectedAdGroupsIds" />
+              </VCardText>
+            </VCard>
+          </v-tabs-window-item>
+        </v-tabs-window>
+      </v-card-text>
+    </v-card>
   </div>
-  <v-card>
-
-    <v-tabs v-model="tab" bg-color="secondary" color="warning" :grow="true">
-      <v-tab
-        base-color="secondary"
-        :text="`(${selectedCampaigns.length}) ` + $t('campaigns')"
-        value="campaigns"
-        variant="elevated"
-      />
-      <v-tab
-        base-color="secondary"
-        :text="`(${selectedAdGroups.length}) ` + $t('ad_groups')"
-        value="ad-groups"
-        variant="elevated"
-      />
-      <v-tab
-        base-color="secondary"
-        :text="$t('ads')"
-        value="ads"
-        variant="elevated"
-      />
-    </v-tabs>
-
-    <v-card-text>
-      <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="campaigns">
-          <VCard>
-            <div class="d-flex align-center justify-space-between ga-4 pa-4">
-              <VCardTitle class="font-weight-medium text-surface-variant pa-0">
-                {{ $t("campaigns") }}
-              </VCardTitle>
-              <VBtn color="warning" rounded :to="{ name: '/campaigns/add' }">
-                <VIcon icon="tabler-circle-plus-filled" />
-                {{ $t("add_campaign") }}
-              </VBtn>
-            </div>
-
-            <VCardText class="pa-4 pt-0">
-              <VDivider />
-              <CampaignsTable
-                :date-range="dateRange"
-                :search="search"
-                @selection-updated="selectedCampaigns = $event"
-              />
-            </VCardText>
-          </VCard>
-        </v-tabs-window-item>
-
-        <v-tabs-window-item value="ad-groups">
-          <VCard>
-            <div class="d-flex align-center justify-space-between ga-4 pa-4">
-              <VCardTitle class="font-weight-medium text-surface-variant pa-0">
-                {{ $t("ad_groups") }}
-              </VCardTitle>
-              <VBtn
-                color="warning"
-                :disabled="selectedCampaigns.length !== 1"
-                rounded
-                :to="addAdGroupRoute"
-              >
-                <VIcon icon="tabler-circle-plus-filled" />
-                {{ $t("add_ad_group") }}
-              </VBtn>
-            </div>
-
-            <VCardText class="pa-4 pt-0">
-              <VDivider />
-              <AdsGroupsTable
-                :campaigns-ids="selectedCampaignsIds"
-                @selection-updated="selectedAdGroups = $event"
-              />
-            </VCardText>
-          </VCard>
-        </v-tabs-window-item>
-
-        <v-tabs-window-item value="ads">
-          <VCard>
-            <div class="d-flex align-center justify-space-between ga-4 pa-4">
-              <VCardTitle class="font-weight-medium text-surface-variant pa-0">
-                {{ $t("ads") }}
-              </VCardTitle>
-              <VBtn
-                color="warning"
-                :disabled="selectedAdGroups.length !== 1"
-                rounded
-                :to="addAdRoute"
-              >
-                <VIcon icon="tabler-circle-plus-filled" />
-                {{ $t("add_ad") }}
-              </VBtn>
-            </div>
-
-            <VCardText class="pa-4 pt-0">
-              <VDivider />
-              <AdsTable :ad-groups-ids="selectedAdGroupsIds" />
-            </VCardText>
-          </VCard>
-        </v-tabs-window-item>
-      </v-tabs-window>
-    </v-card-text>
-  </v-card>
 </template>
 <style lang="scss" scoped>
 .container {
