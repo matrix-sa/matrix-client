420ec158fc5d86c03d94e9bd36ccbefdaeff1ca3
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-11-06 11:54:41 +0200

    Create Notifications

---
+++ src/components/notifications/Notifications.vue
@@ -0,0 +1,66 @@
+<script setup>
+  import NotificationsService from '@/services/notifications-service'
+  import { usePagination, useRequest } from 'vue-request'
+  import { useSnackbarStore } from '@/stores/useSnackBarStore'
+  import NotificationItem from './NotificationItem.vue'
+
+  const notifications = ref(null)
+  const totalCount = ref(0)
+  const pageSize = ref()
+  const { show } = useSnackbarStore()
+
+  const options = ref({
+    page: 1,
+    itemsPerPage: 10,
+    sortBy: [],
+    groupBy: [],
+    search: undefined,
+  })
+
+  const getQuery = params => {
+    const query = new URLSearchParams()
+
+    query.append('PageSize', options.value.itemsPerPage)
+    query.append('Page', options.value.page)
+
+    return query
+  }
+
+  const { run, loading: loadingNotifications } = usePagination(
+    params => NotificationsService.get(getQuery(params)),
+    {
+      manual: true,
+      onSuccess: res => {
+        const { data, error, messages } = res.data
+        if (error) {
+          show(messages[0], 'error')
+          return
+        }
+        notifications.value = data.items
+        options.value.page = data.current_page
+        pageSize.value = data.page_size
+        totalCount.value = data.total_count
+      },
+      onError: err => {
+        console.log(err)
+      },
+    }
+  )
+
+  run()
+
+</script>
+
+<template>
+  <div>
+    <v-overlay v-model="loadingNotifications" class="align-center justify-center" persistent>
+      <v-progress-circular color="primary" indeterminate size="50" :width="7" />
+    </v-overlay>
+    <NotificationItem
+      v-for="notification in notifications"
+      v-if="notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
---
+++ src/pages/notifications/index.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import { useI18n } from 'vue-i18n'
+  import { useBreadcrumbsStore } from '@/stores/useBreadcrumbsStore'
+  import Header from '@/components/notifications/Header.vue'
+
+  const { t, locale } = useI18n()
+  const { update } = useBreadcrumbsStore()
+
+  watch(
+    locale,
+    () => {
+      update([
+        {
+          title: t('notifications'),
+          active: false,
+          to: '/notifications/',
+        },
+      ])
+    },
+    { immediate: true }
+  )
+
+</script>
+
+<template>
+  <div class="d-flex flex-column wrapper mb-8">
+
+    <Header />
+    <hr class="separator">
+    <Notifications />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$borderColor: rgba(31, 22, 37, 0.1);
+
+.wrapper {
+  gap: 16px;
+  background-color: #fff;
+  padding: 24px;
+  border-radius: 16px;
+
+  .separator {
+    border-color: $borderColor;
+  }
+}
+</style>
