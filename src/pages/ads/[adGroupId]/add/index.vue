076294d94146834e89c586af09a959f66045166e
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-11-19 17:01:00 +0200

    Finish Ad RecommendationList

--- src/pages/ads/[adGroupId]/add/index.vue
+++ src/pages/ads/[adGroupId]/add/index.vue
@@ -1,22 +1,53 @@
 <script setup>
-  const route = useRoute()
-  const adGroupId = route.params.adGroupId
-  const platform = route.query.platform?.toLowerCase()
+const route = useRoute()
+const adGroupId = route.params.adGroupId
+const platform = route.query.platform?.toLowerCase()
+
+const recommendations = ref([
+  {
+    id: 1,
+    text: "my_answers_title"
+  },
+  {
+    id: 2,
+    text: "my_answers_title"
+  },
+  {
+    id: 3,
+    text: "my_answers_title"
+  }, {
+    id: 4,
+    text: "my_answers_title"
+  }
+])
 </script>
 <template>
-  <AdFormFieldsTiktok
-    v-if="platform === 'tiktok'"
-    :ad-group-id="adGroupId"
-    :platform="platform"
-  />
-  <AdFormFieldsSnapchat
-    v-if="platform === 'snapchat'"
-    :ad-group-id="adGroupId"
-    :platform="platform"
-  />
-  <AdFormFieldsX
-    v-if="platform === 'twitter'"
-    :ad-group-id="adGroupId"
-    :platform="platform"
-  />
+
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="8" class="ps-0 form-container">
+          <AdFormFieldsTiktok v-if="platform === 'tiktok'" :ad-group-id="adGroupId" :platform="platform" />
+          <AdFormFieldsSnapchat v-if="platform === 'snapchat'" :ad-group-id="adGroupId" :platform="platform" />
+          <AdFormFieldsX v-if="platform === 'twitter'" :ad-group-id="adGroupId" :platform="platform" />
+
+        </v-col>
+        <v-col cols="4" class="pe-0 pt-0">
+          <div class="main-container">
+            <RecommendationList :recommendations="recommendations" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+
 </template>
+
+<style scoped>
+.form-container,
+.main-container {
+  background-color: white;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+</style>
