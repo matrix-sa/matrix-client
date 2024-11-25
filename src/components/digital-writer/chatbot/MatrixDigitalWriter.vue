4e9efd401920e35fcbd7aaa2d35f75e86aacd078
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2024-11-25 14:41:29 +0300

    Display Markdown correctly

--- src/components/digital-writer/chatbot/MatrixDigitalWriter.vue
+++ src/components/digital-writer/chatbot/MatrixDigitalWriter.vue
@@ -1,8 +1,8 @@
 <script setup>
   import { useI18n } from 'vue-i18n'
   import matrixBot from '@/assets/digital-writer/Matrux_bot.svg'
-
-  defineProps({
+  import { marked } from 'marked'
+  const props = defineProps({
     message: {
       type: String,
       default: '',
@@ -11,23 +11,31 @@
 
   const { t } = useI18n()
 
+  onMounted(() => {
+    document.getElementById('digital-writer-wrapper').innerHTML = marked.parse(
+      props.message
+    )
+  })
 </script>
 
 <template>
   <div class="title-container d-flex flex-column">
     <div class="d-flex align-center title-wrapper">
       <img alt="chatbot" :src="matrixBot">
-      <span>
-        {{ t('matrix_digital_writer') }}</span>
+      <span> {{ t("matrix_digital_writer") }}</span>
     </div>
-    <div class="content-wrapper" v-html="message.replace(/\n/g, '<br>')" />
+    <div
+      id="digital-writer-wrapper"
+      class="content-wrapper"
+      v-html="message.replace(/\n/g, '<br>')"
+    />
   </div>
 </template>
 
 <style scoped lang="scss">
 .title-container {
-  background-color: #EFECF6 !important;
-  border: 1px solid #CFCFCF !important;
+  background-color: #efecf6 !important;
+  border: 1px solid #cfcfcf !important;
   padding: 16px;
   border-radius: 12px;
   gap: 16px;
@@ -36,16 +44,22 @@
     gap: 8px;
 
     span {
-      color: #000
+      color: #000;
     }
   }
 
   .content-wrapper {
-    background-color: #F8F7FA;
+    background-color: #f8f7fa;
     padding: 16px;
     border-radius: 8px;
-    color: #1F1625
+    color: #1f1625;
   }
-
+}
+</style>
+<style lang="scss">
+#digital-writer-wrapper {
+  li {
+    list-style-position: inside;
+  }
 }
 </style>
