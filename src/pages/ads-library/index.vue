44171ec9d296a5567b472460de7bad9aced40fca
Sayed <sayedshehata33@gmail.com>
2025-01-13 12:43:34 +0200

    feat: :sparkles: Implement breadcrumbs for Ads Library page

--- src/pages/ads-library/index.vue
+++ src/pages/ads-library/index.vue
@@ -1,3 +1,26 @@
+<script  setup>
+  import { watch } from 'vue'
+  import { useI18n } from 'vue-i18n'
+  import { useBreadcrumbsStore } from '@/stores/useBreadcrumbsStore'
+
+  const { t, locale } = useI18n()
+  const { update } = useBreadcrumbsStore()
+
+  watch(
+    locale,
+    () => {
+      update([
+        {
+          title: t('ads_library'),
+          active: true,
+          to: '/ads-library',
+        },
+      ])
+    },
+    { immediate: true }
+  )
+</script>
+
 <template>
   <div>
     Ads library
